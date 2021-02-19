d39d0cd53fea75c158146a8926b245d3bf777c7b
devkhaldi <dev.khaldi@gmail.com>
2021-02-19 23:17:22 +0100

    Location info

--- src/components/LocaionInfoBox.js
+++ src/components/LocaionInfoBox.js
@@ -1,7 +1,17 @@
-import React from 'react'
-
-const LocaionInfoBox = () => {
-  return <div></div>
+const LocaionInfoBox = ({ info }) => {
+  return (
+    <div className='location-info'>
+      <h2>Location info</h2>
+      <ul>
+        <li>
+          ID : <strong>{info.id}</strong>
+        </li>
+        <li>
+          TITLE : <strong>{info.title}</strong>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default LocaionInfoBox
