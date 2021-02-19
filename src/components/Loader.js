1e34da4c8098cf36d8095b3ecab5cfc235eb9c9f
devkhaldi <dev.khaldi@gmail.com>
2021-02-19 23:59:39 +0100

    change loadr

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = () => {
   return (
     <div className='loader'>
-      <img src='https://flevix.com/wp-content/uploads/2019/07/Comp-2.gif' alt='' />
+      <img src='https://i.pinimg.com/originals/a2/de/bf/a2debfb85547f48c3a699423ba75f321.gif' alt='' />
       <h1>Loading data ...</h1>
     </div>
   )
