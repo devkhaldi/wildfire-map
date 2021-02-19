2e1d8801dd90e20099bebcfc451c28bb55153d47
devkhaldi <dev.khaldi@gmail.com>
2021-02-19 23:54:23 +0100

    Loader

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className='loader'>
+      <img src='' alt='' />
+      <h1>Loading data ...</h1>
+    </div>
+  )
+}
+
+export default Loader
